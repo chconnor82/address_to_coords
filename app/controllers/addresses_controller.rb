ec55da52cf93d653592b3bf666a58d7a85fa5374
Charlie Connor <chconnor@gmail.com>
2014-04-27 18:29:42 -0500

    Second Attempt
    Doing Challenge API 1

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -7,14 +7,15 @@ class AddressesController < ApplicationController
     @url_safe_address = URI.encode(@address)
 
     # # Your code goes here.
-    open-uri = ["http://maps.googleapis.com/maps/api/geocode/json?address=the+corner+of+Foster+and+Sheridian&sensor=true"]
-    raw_data = ["open-uri"].read
-    parsed_data.key = ["JSON"].parse["raw_data"]
-    results = parsed_data["results"]
-    first = ["results"] [0]
-    geometry = first["geometry"]
-    location = geometry["location"]
-    latitude = parsed_data["results"][0]["geometry"]["locaiton"]["lat"]
-    longitude = parsed_data["results"][0]["geometry"]["locaiton"]["lng"]
-  end
+
+
+@url_safe_address = "http://maps.googleapis.com/maps/api/geocode/json?address=the+corner+of+foster+and+sheridan&sensor=false"
+# require 'open["URI.encode(@address)"]'
+raw_data = 'open' "@url_safe_address.read"
+parsed_data = JSON.parse(raw_data)
+
+@latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+@longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+
+ end
 end
