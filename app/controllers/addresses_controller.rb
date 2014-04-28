e74c1abc9086f98aed2209648b8d468f8e48308c
Charlie Connor <chconnor@gmail.com>
2014-04-27 20:18:13 -0500

    Final Phase 1 - API Exercise
    Final Phase 1 - API Exercise - mistake with formatting…

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -11,7 +11,7 @@ class AddressesController < ApplicationController
 
 @url_safe_address = "http://maps.googleapis.com/maps/api/geocode/json?address=the+corner+of+foster+and+sheridan&sensor=false"
 # require 'open["URI.encode(@address)"]'
-raw_data = 'open' "@url_safe_address.read"
+raw_data = open(@url_safe_address).read
 parsed_data = JSON.parse(raw_data)
 
 @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
