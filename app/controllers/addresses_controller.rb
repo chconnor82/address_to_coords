6e7395d90e821608d6362403db49588e436d7345
Charlie Connor <chconnor@gmail.com>
2014-05-04 23:06:17 -0500

    Final Attempt
    Cannot get past nil and having issues with if statements

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,19 +3,21 @@ require 'json'
 
 class AddressesController < ApplicationController
   def fetch_coordinates
-    @address = "the corner of Foster and Sheridan"
-    @url_safe_address = URI.encode(@address)
-
+    @address = params["address"]
+      if (@address) > .nil
+      (@address)
+    else
+      "Other:" ["Please_fill_out_form"]
+    end
     # # Your code goes here.
+ @url_safe_address = URI.encode(@address)
 
-
-@url_safe_address = "http://maps.googleapis.com/maps/api/geocode/json?address=the+corner+of+foster+and+sheridan&sensor=false"
+url= "http://maps.googleapis.com/maps/api/geocode/json?address=the+corner+of+foster+and+sheridan&sensor=false"
 # require 'open["URI.encode(@address)"]'
-raw_data = open(@url_safe_address).read
+raw_data = open(url).read
 parsed_data = JSON.parse(raw_data)
 
 @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
 @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 AddressToCoords::Application.routes.draw do
   get("/weather", { :controller => "addresses", :action => "fetch_coordinates" })
+
+  get("/weather/:address", { :controller => "addresses", :action => "fetch_coordinates" })
+
+
 end
